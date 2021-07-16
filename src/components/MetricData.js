c99bb2e6af0eee9f659610d1746ff9e1d555dbfc
unknown <prashantkbm@gmail.com>
2021-07-16 15:21:27 +0530

    Initial commit

---
+++ src/components/MetricData.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+const MetricData = () => {
+
+
+    return(
+        <div>
+            <Grid container spacing={1}>
+                Chart component
+            </Grid>
+        </div>
+    )
+}
+
+export default MetricData;
