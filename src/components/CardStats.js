31efec441089989d0d2eee9ee39f01f9ebbbc704
unknown <prashantkbm@gmail.com>
2021-07-16 18:38:52 +0530

    Created placeholder components with basic design

---
+++ src/components/CardStats.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    details: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    content: {
+      flex: '1 0 auto',
+    },
+    cover: {
+      width: 151,
+    },
+    
+  }));
+
+const CardStats = () => {
+    const classes = useStyles();
+    //sconst theme = useTheme();
+    return(
+        <Card className={classes.root}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            Live From Space
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            Mac Miller
+          </Typography>
+        </CardContent>
+        
+      </div>
+      </Card>
+    )
+}
+
+export default CardStats;
--- src/components/MetricData.js
+++ src/components/MetricData.js
@@ -1,13 +1,71 @@
 import React from 'react';
+// import { useQuery, useSubscription } from 'urql';
+// import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import CardStats from './CardStats';
+import MultiSelect from './MultiSelect';
+// import { width } from '@material-ui/system';
+// import * as actions from '../store/actions';
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      margin: '10px'
+    },
+    paper: {
+       padding: '0 10px 10px',
+      color: theme.palette.text.secondary,
+      float: 'right',
+      width: '30%',
+      marginBottom: '10px'
+
+    },
+    charContainer: {
+        padding: '10px',
+        color: theme.palette.text.secondary,
+        height: '100%'
+    },
+    item: {
+        marginBottom: '10px'
+    }
+  }));
+
 
 const MetricData = () => {
-
+    const classes = useStyles();
 
     return(
-        <div>
+        <div className={classes.root}>
             <Grid container spacing={1}>
-                Chart component
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}> <MultiSelect /> </Paper>
+                </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+                <Grid item xs={2} className={classes.item}>
+                    <CardStats />
+                </Grid>
+                <Grid item xs={2}>
+                    <CardStats />
+                </Grid>
+                <Grid item xs={2}>
+                    <CardStats />
+                </Grid>
+                <Grid item xs={2}>
+                    <CardStats />
+                </Grid>
+                <Grid item xs={2}>
+                    <CardStats />
+                </Grid>
+                <Grid item xs={2}>
+                    <CardStats />
+                </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+                <Grid item xs={12}>
+                    <Paper className={classes.charContainer}> Drop Down </Paper>
+                </Grid>
             </Grid>
         </div>
     )
